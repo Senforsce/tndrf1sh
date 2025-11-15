1c20edaf961c59927bb48e03ec763e556321146e
Abdoul sy (Senforsce) <dreescan+senforsce@gmail.com>
2025-11-16 00:03:52 +0100

    chore(HDT-1.0.0(beta)): Making sure that the previews are injected automatically by ./hdt

--- web/inject_previews.go
+++ web/inject_previews.go
@@ -1,78 +1,19 @@
 package main
 
 import (
-	"github.com/senforsce/owl/connected/programmelist"
-	"github.com/senforsce/owl/htm/about"
-	"github.com/senforsce/owl/htm/abouthero"
-	"github.com/senforsce/owl/htm/aboutlogo"
-	"github.com/senforsce/owl/htm/aboutquote"
-	"github.com/senforsce/owl/htm/adminsidebar"
-	"github.com/senforsce/owl/htm/admintopbar"
-	"github.com/senforsce/owl/htm/appointment"
-	"github.com/senforsce/owl/htm/contactctapills"
-	"github.com/senforsce/owl/htm/contactform"
-	"github.com/senforsce/owl/htm/contactformwrapper"
-	"github.com/senforsce/owl/htm/dashboardtopstats"
-	"github.com/senforsce/owl/htm/eventlist"
-	"github.com/senforsce/owl/htm/feature"
-	"github.com/senforsce/owl/htm/footer"
-	"github.com/senforsce/owl/htm/fullcalendar"
-	"github.com/senforsce/owl/htm/header"
-	"github.com/senforsce/owl/htm/headercarousel"
-	"github.com/senforsce/owl/htm/hero"
-	"github.com/senforsce/owl/htm/monthlystats"
-	"github.com/senforsce/owl/htm/myvalues"
-	"github.com/senforsce/owl/htm/navbar"
-	"github.com/senforsce/owl/htm/ocean"
-	"github.com/senforsce/owl/htm/quickchat"
-	"github.com/senforsce/owl/htm/service"
-	"github.com/senforsce/owl/htm/sitevisits"
-	"github.com/senforsce/owl/htm/spinner"
-	"github.com/senforsce/owl/htm/subscriptions"
-	"github.com/senforsce/owl/htm/team"
-	"github.com/senforsce/owl/htm/testimonial"
-	"github.com/senforsce/owl/htm/topbar"
-	"github.com/senforsce/owl/htm/values"
-	"github.com/senforsce/owl/htm/vibelist"
-
 	"github.com/senforsce/tndr0cean/router"
+	"github.com/senforsce/owl/htm/about"
+	"github.com/senforsce/owl/htm/ocean"
+	"github.com/senforsce/tndrf1sh/web/owl/abdoulsylogo"
+	
 )
 
 func WithHTMXPreviews(app *router.Tndr0cean) error {
-	app.Get("/preview/about", about.Preview)
-	app.Get("/preview/abouthero", abouthero.Preview)
-	app.Get("/preview/aboutquote", aboutquote.Preview)
-	app.Get("/preview/aboutlogo", aboutlogo.Preview)
-	app.Get("/preview/adminsidebar", adminsidebar.Preview)
-	app.Get("/preview/admintopbar", admintopbar.Preview)
-	app.Get("/preview/appointment", appointment.Preview)
-	app.Get("/preview/contactctapills", contactctapills.Preview)
-	app.Get("/preview/contactform", contactform.Preview)
-	app.Get("/preview/contactformwrapper", contactformwrapper.Preview)
-	app.Get("/preview/dashboardtopstats", dashboardtopstats.Preview)
-	app.Get("/preview/eventlist", eventlist.Preview)
-	app.Get("/preview/feature", feature.Preview)
-	app.Get("/preview/footer", footer.Preview)
-	app.Get("/preview/fullcalendar", fullcalendar.Preview)
-	app.Get("/preview/header", header.Preview)
-	app.Get("/preview/headercarousel", headercarousel.Preview)
-	app.Get("/preview/headerstats", hero.Preview)
-	app.Get("/preview/monthlystats", monthlystats.Preview)
-	app.Get("/preview/myvalues", myvalues.Preview)
-	app.Get("/preview/navbar", navbar.Preview)
-	app.Get("/preview/ocean", ocean.Preview)
-
-	app.Get("/preview/quickchat", quickchat.Preview)
-	app.Get("/preview/programmelist", programmelist.Preview)
-	app.Get("/preview/service", service.Preview)
-	app.Get("/preview/sitevisits", sitevisits.Preview)
-	app.Get("/preview/spinner", spinner.Preview)
-	app.Get("/preview/subscriptions", subscriptions.Preview)
-	app.Get("/preview/team", team.Preview)
-	app.Get("/preview/testimonial", testimonial.Preview)
-	app.Get("/preview/topbar", topbar.Preview)
-	app.Get("/preview/values", values.Preview)
-	app.Get("/preview/vibelist", vibelist.Preview)
+	app.Get("/preview/about", about.Preview) 
+	app.Get("/preview/ocean", ocean.Preview) 
+	app.Get("/preview/abdoulsylogo", abdoulsylogo.Preview) 
+	
 
 	return nil
 }
+	
